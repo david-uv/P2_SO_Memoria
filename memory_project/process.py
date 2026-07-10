204619eaa4f55913d2291f6cd189eca6e1dc780f
David <david.grueso@correounivalle.edu.co>
2026-07-10 11:09:30 -0500

    Cleaning: deleting duplicated files in the root

--- memory_project/process.py
+++
@@ -1,7 +0,0 @@
-class Process:
-    def __init__(self, pid: str, memory_required: int):
-        self.pid = pid
-        self.memory_required = memory_required
-
-    def __str__(self):
-        return f"{self.pid} ({self.memory_required} MB)"
